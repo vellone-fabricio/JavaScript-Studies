42161f90be70cfe93b0790e5f8053dbc32ecac13
Fabricio V <fabriciovellone@gmail.com>
2021-02-25 01:28:37 +0100

    second function implementation

--- etudes1.js
+++ etudes1.js
@@ -49,3 +49,46 @@ testA([-55.9, 193.99]);
 testA([555, 193.99]);
 
 
+//-----------------------------------------------------------------------------
+console.log("\t\bSTUDYING FUNCTION 2:\n");
+function squareNumbers(interval){
+  let allSquare = [];
+  let arr = [];
+
+  if (interval[0] > interval[1]){
+    let aux = interval[0];
+    interval[0] = interval[1];
+    interval[1] = aux;
+  }
+  interval[0] = Math.ceil(interval[0]);
+  interval[1] = Math.floor(interval[1]);
+
+
+  for (let i = interval[0]; i<=interval[1]; i++){
+    arr.push(i);
+  }
+
+  for (let i=0; i<arr.length; i++){
+    if (arr[i] > 0){
+      if (Number.isInteger(Math.sqrt(arr[i]))) allSquare.push(arr[i]);
+    }
+  }
+
+  return allSquare;
+}
+
+
+function testB(interval){
+  console.log(`Outputs for our function 'Square Numbers', between the interval [${interval[0]}, ${interval[1]}]:`);
+
+  const result = squareNumbers(interval);
+
+  console.log(`The values which are Square numbers are: ${result}\nSo the number of Square numbers in this array is ${result.length}\n`);
+}
+
+testB([1, 49]);
+testB([4.5, 49.9]);
+testB([-11, 20]);
+testB([-51, 0]);
+testB([-55.9, 193.99]);
+testB([555, 193.99]);
